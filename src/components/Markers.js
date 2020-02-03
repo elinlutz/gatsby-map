25aa420bb02e882be38b881b8353efb6900056f8
Elin Lütz <lutz.elin@gmail.com>
2020-02-03 14:33:03 +0100

    Add custon icon to markers

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
 import { Marker, Popup } from 'react-leaflet'
+import L from 'leaflet'
 
 import { units } from 'data/units.js'
+
+var virusIcon = L.icon({
+  iconUrl: require('assets/icons/bacteria.png'),
+  // shadowUrl: 'leaf-shadow.png',
+
+  iconSize: [35, 35], // size of the icon
+  // shadowSize: [50, 64], // size of the shadow
+  // iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -10] // point from which the popup should open relative to the iconAnchor
+})
 
 const Markers = ({ onClick }) => {
   const [clicked, setClicked] = useState(false)
 
   return units.map(unit => {
     return (
-      <Marker key={unit.id} position={[unit.lat, unit.lng]}>
-        <Popup onClose={() => setClicked(false)}>
+      <Marker key={unit.id} icon={virusIcon} position={[unit.lat, unit.lng]}>
+        <Popup icon={virusIcon} onClose={() => setClicked(false)}>
           <a
             onClick={() => {
               setClicked(true)
