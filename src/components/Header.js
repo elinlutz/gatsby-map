c3a5f1c09340f434562a41008de247a8e1afeb47
Elin Lütz <lutz.elin@gmail.com>
2020-02-29 15:18:03 +0100

    Add bicon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Button from '@material-ui/core/Button'
+import MapIcon from '@material-ui/icons/Map'
+import RoomIcon from '@material-ui/icons/Room'
+import BubbleChart from '@material-ui/icons/BubbleChart'
 import Img from 'gatsby-image'
 import { graphql } from 'gatsby'
 import logo from '../assets/icons/corona.png'
@@ -15,19 +18,19 @@ const Header = () => {
         <p>
           <div className="titleContainer">
             <Link className="title" to="/">
-              {/* <img className="logo" src={logo} /> */}
+              <BubbleChart className="icon" />
               Coronakartan
             </Link>
           </div>
         </p>
         <ul>
           <li>
-            <Button className="header navigationLink">
+            <Button className="navigationLink">
               <Link to="/nyheter/">Tidslinje</Link>
             </Button>
           </li>
           <li>
-            <Button className="header navigationLink">
+            <Button className="navigationLink">
               <Link to="/info/">Om</Link>
             </Button>
           </li>
