f86bac84eb24261d0a77b62f2564adf76ba76d26
Elin Lütz <lutz.elin@gmail.com>
2020-03-12 08:14:18 +0100

    Add image

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       'Karta över rapporterade fall av coronaviruset COVID-19 i Sverige',
     keywords: ['Coronavirus', 'Karta', 'covid-19', 'Sverige'],
     siteUrl: 'https://www.coronakartan.se',
-    image: 'static/coronakartan.png',
+    image: '/coronakartan.png',
     twitterUsername: '@utzle',
     menuLinks: [
       {
