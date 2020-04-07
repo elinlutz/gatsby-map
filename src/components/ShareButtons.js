6298dfeaf3a5f5cc80170a3dbb87e88e94df5a76
Elin Lütz <lutz.elin@gmail.com>
2020-04-07 12:26:42 +0200

    Smaller sharebtns

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -20,7 +20,7 @@ const ShareButtons = () => {
               <path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z" />
             </svg>
           </div>
-          Dela
+          Dela sidan
         </div>
       </OutboundLink>
 
