c6a04410bd8525fde33a0ce1c379b4536bffcc7c
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-04-09 10:56:07 +0200

    returrn

--- src/components/Map.js
+++ src/components/Map.js
@@ -48,8 +48,6 @@ const Map = props => {
     attributionControl: true,
     ...rest
   }
-  console.log(mapRef)
-  console.log(mapRef.getZoom())
   return (
     <div className={mapClassName}>
       <BaseMap ref={mapRef} {...mapSettings}>
