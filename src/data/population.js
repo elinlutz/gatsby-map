7739d6f52b50d4e136579ed3d587f92a191ac0fd
Elin Lütz <lutz.elin@gmail.com>
2020-04-11 12:41:54 +0200

    Add population dataset

---
+++ src/data/population.js
@@ -0,0 +1,1058 @@
+;[
+  {
+    country: '',
+    population: ''
+  },
+  {
+    country: 'Aruba',
+    population: 105845
+  },
+  {
+    country: 'Afghanistan',
+    population: 37172386
+  },
+  {
+    country: 'Angola',
+    population: 30809762
+  },
+  {
+    country: 'Albania',
+    population: 2866376
+  },
+  {
+    country: 'Andorra',
+    population: 77006
+  },
+  {
+    country: 'Arab World',
+    population: 419790588
+  },
+  {
+    country: 'United Arab Emirates',
+    population: 9630959
+  },
+  {
+    country: 'Argentina',
+    population: 44494502
+  },
+  {
+    country: 'Armenia',
+    population: 2951776
+  },
+  {
+    country: 'American Samoa',
+    population: 55465
+  },
+  {
+    country: 'Antigua and Barbuda',
+    population: 96286
+  },
+  {
+    country: 'Australia',
+    population: 24982688
+  },
+  {
+    country: 'Austria',
+    population: 8840521
+  },
+  {
+    country: 'Azerbaijan',
+    population: 9939800
+  },
+  {
+    country: 'Burundi',
+    population: 11175378
+  },
+  {
+    country: 'Belgium',
+    population: 11433256
+  },
+  {
+    country: 'Benin',
+    population: 11485048
+  },
+  {
+    country: 'Burkina Faso',
+    population: 19751535
+  },
+  {
+    country: 'Bangladesh',
+    population: 161356039
+  },
+  {
+    country: 'Bulgaria',
+    population: 7025037
+  },
+  {
+    country: 'Bahrain',
+    population: 1569439
+  },
+  {
+    country: 'Bahamas, The',
+    population: 385640
+  },
+  {
+    country: 'Bosnia and Herzegovina',
+    population: 3323929
+  },
+  {
+    country: 'Belarus',
+    population: 9483499
+  },
+  {
+    country: 'Belize',
+    population: 383071
+  },
+  {
+    country: 'Bermuda',
+    population: 63973
+  },
+  {
+    country: 'Bolivia',
+    population: 11353142
+  },
+  {
+    country: 'Brazil',
+    population: 209469333
+  },
+  {
+    country: 'Barbados',
+    population: 286641
+  },
+  {
+    country: 'Brunei Darussalam',
+    population: 428962
+  },
+  {
+    country: 'Bhutan',
+    population: 754394
+  },
+  {
+    country: 'Botswana',
+    population: 2254126
+  },
+  {
+    country: 'Central African Republic',
+    population: 4666377
+  },
+  {
+    country: 'Canada',
+    population: 37057765
+  },
+  {
+    country: 'Central Europe and the Baltics',
+    population: 102511922
+  },
+  {
+    country: 'Switzerland',
+    population: 8513227
+  },
+  {
+    country: 'Channel Islands',
+    population: 170499
+  },
+  {
+    country: 'Chile',
+    population: 18729160
+  },
+  {
+    country: 'China',
+    population: 1392730000
+  },
+  {
+    country: "Cote d'Ivoire",
+    population: 25069229
+  },
+  {
+    country: 'Cameroon',
+    population: 25216237
+  },
+  {
+    country: 'Congo, Dem. Rep.',
+    population: 84068091
+  },
+  {
+    country: 'Congo, Rep.',
+    population: 5244363
+  },
+  {
+    country: 'Colombia',
+    population: 49648685
+  },
+  {
+    country: 'Comoros',
+    population: 832322
+  },
+  {
+    country: 'Cabo Verde',
+    population: 543767
+  },
+  {
+    country: 'Costa Rica',
+    population: 4999441
+  },
+  {
+    country: 'Caribbean small states',
+    population: 7358965
+  },
+  {
+    country: 'Cuba',
+    population: 11338138
+  },
+  {
+    country: 'Curacao',
+    population: 159800
+  },
+  {
+    country: 'Cayman Islands',
+    population: 64174
+  },
+  {
+    country: 'Cyprus',
+    population: 1189265
+  },
+  {
+    country: 'Czech Republic',
+    population: 10629928
+  },
+  {
+    country: 'Germany',
+    population: 82905782
+  },
+  {
+    country: 'Djibouti',
+    population: 958920
+  },
+  {
+    country: 'Dominica',
+    population: 71625
+  },
+  {
+    country: 'Denmark',
+    population: 5793636
+  },
+  {
+    country: 'Dominican Republic',
+    population: 10627165
+  },
+  {
+    country: 'Algeria',
+    population: 42228429
+  },
+  {
+    country: 'East Asia & Pacific (excluding high income)',
+    population: 2081651801
+  },
+  {
+    country: 'Early-demographic dividend',
+    population: 3249140605
+  },
+  {
+    country: 'East Asia & Pacific',
+    population: 2328220870
+  },
+  {
+    country: 'Europe & Central Asia (excluding high income)',
+    population: 417797257
+  },
+  {
+    country: 'Europe & Central Asia',
+    population: 918793590
+  },
+  {
+    country: 'Ecuador',
+    population: 17084357
+  },
+  {
+    country: 'Egypt, Arab Rep.',
+    population: 98423595
+  },
+  {
+    country: 'Euro area',
+    population: 341783171
+  },
+  {
+    country: 'Eritrea',
+    population: ''
+  },
+  {
+    country: 'Spain',
+    population: 46796540
+  },
+  {
+    country: 'Estonia',
+    population: 1321977
+  },
+  {
+    country: 'Ethiopia',
+    population: 109224559
+  },
+  {
+    country: 'European Union',
+    population: 446786293
+  },
+  {
+    country: 'Fragile and conflict affected situations',
+    population: 743958386
+  },
+  {
+    country: 'Finland',
+    population: 5515525
+  },
+  {
+    country: 'Fiji',
+    population: 883483
+  },
+  {
+    country: 'France',
+    population: 66977107
+  },
+  {
+    country: 'Faroe Islands',
+    population: 48497
+  },
+  {
+    country: 'Micronesia, Fed. Sts.',
+    population: 112640
+  },
+  {
+    country: 'Gabon',
+    population: 2119275
+  },
+  {
+    country: 'United Kingdom',
+    population: 66460344
+  },
+  {
+    country: 'Georgia',
+    population: 3726549
+  },
+  {
+    country: 'Ghana',
+    population: 29767108
+  },
+  {
+    country: 'Gibraltar',
+    population: 33718
+  },
+  {
+    country: 'Guinea',
+    population: 12414318
+  },
+  {
+    country: 'Gambia, The',
+    population: 2280102
+  },
+  {
+    country: 'Guinea-Bissau',
+    population: 1874309
+  },
+  {
+    country: 'Equatorial Guinea',
+    population: 1308974
+  },
+  {
+    country: 'Greece',
+    population: 10731726
+  },
+  {
+    country: 'Grenada',
+    population: 111454
+  },
+  {
+    country: 'Greenland',
+    population: 56025
+  },
+  {
+    country: 'Guatemala',
+    population: 17247807
+  },
+  {
+    country: 'Guam',
+    population: 165768
+  },
+  {
+    country: 'Guyana',
+    population: 779004
+  },
+  {
+    country: 'High income',
+    population: 1210312147
+  },
+  {
+    country: 'Hong Kong SAR, China',
+    population: 7451000
+  },
+  {
+    country: 'Honduras',
+    population: 9587522
+  },
+  {
+    country: 'Heavily indebted poor countries (HIPC)',
+    population: 780234406
+  },
+  {
+    country: 'Croatia',
+    population: 4087843
+  },
+  {
+    country: 'Haiti',
+    population: 11123176
+  },
+  {
+    country: 'Hungary',
+    population: 9775564
+  },
+  {
+    country: 'IBRD only',
+    population: 4772284113
+  },
+  {
+    country: 'IDA & IBRD total',
+    population: 6412522234
+  },
+  {
+    country: 'IDA total',
+    population: 1640238121
+  },
+  {
+    country: 'IDA blend',
+    population: 555830605
+  },
+  {
+    country: 'Indonesia',
+    population: 267663435
+  },
+  {
+    country: 'IDA only',
+    population: 1084407516
+  },
+  {
+    country: 'Isle of Man',
+    population: 84077
+  },
+  {
+    country: 'India',
+    population: 1352617328
+  },
+  {
+    country: 'Ireland',
+    population: 4867309
+  },
+  {
+    country: 'Iran, Islamic Rep.',
+    population: 81800269
+  },
+  {
+    country: 'Iraq',
+    population: 38433600
+  },
+  {
+    country: 'Iceland',
+    population: 352721
+  },
+  {
+    country: 'Israel',
+    population: 8882800
+  },
+  {
+    country: 'Italy',
+    population: 60421760
+  },
+  {
+    country: 'Jamaica',
+    population: 2934855
+  },
+  {
+    country: 'Jordan',
+    population: 9956011
+  },
+  {
+    country: 'Japan',
+    population: 126529100
+  },
+  {
+    country: 'Kazakhstan',
+    population: 18272430
+  },
+  {
+    country: 'Kenya',
+    population: 51393010
+  },
+  {
+    country: 'Kyrgyz Republic',
+    population: 6322800
+  },
+  {
+    country: 'Cambodia',
+    population: 16249798
+  },
+  {
+    country: 'Kiribati',
+    population: 115847
+  },
+  {
+    country: 'St. Kitts and Nevis',
+    population: 52441
+  },
+  {
+    country: 'Korea, Rep.',
+    population: 51606633
+  },
+  {
+    country: 'Kuwait',
+    population: 4137309
+  },
+  {
+    country: 'Latin America & Caribbean (excluding high income)',
+    population: 609013934
+  },
+  {
+    country: 'Lao PDR',
+    population: 7061507
+  },
+  {
+    country: 'Lebanon',
+    population: 6848925
+  },
+  {
+    country: 'Liberia',
+    population: 4818977
+  },
+  {
+    country: 'Libya',
+    population: 6678567
+  },
+  {
+    country: 'St. Lucia',
+    population: 181889
+  },
+  {
+    country: 'Latin America & Caribbean',
+    population: 641357515
+  },
+  {
+    country: 'Least developed countries: UN classification',
+    population: 1009662578
+  },
+  {
+    country: 'Low income',
+    population: 705417321
+  },
+  {
+    country: 'Liechtenstein',
+    population: 37910
+  },
+  {
+    country: 'Sri Lanka',
+    population: 21670000
+  },
+  {
+    country: 'Lower middle income',
+    population: 3022905169
+  },
+  {
+    country: 'Low & middle income',
+    population: 6383958209
+  },
+  {
+    country: 'Lesotho',
+    population: 2108132
+  },
+  {
+    country: 'Late-demographic dividend',
+    population: 2288665963
+  },
+  {
+    country: 'Lithuania',
+    population: 2801543
+  },
+  {
+    country: 'Luxembourg',
+    population: 607950
+  },
+  {
+    country: 'Latvia',
+    population: 1927174
+  },
+  {
+    country: 'Macao SAR, China',
+    population: 631636
+  },
+  {
+    country: 'St. Martin (French part)',
+    population: 37264
+  },
+  {
+    country: 'Morocco',
+    population: 36029138
+  },
+  {
+    country: 'Monaco',
+    population: 38682
+  },
+  {
+    country: 'Moldova',
+    population: 2706049
+  },
+  {
+    country: 'Madagascar',
+    population: 26262368
+  },
+  {
+    country: 'Maldives',
+    population: 515696
+  },
+  {
+    country: 'Middle East & North Africa',
+    population: 448912859
+  },
+  {
+    country: 'Mexico',
+    population: 126190788
+  },
+  {
+    country: 'Marshall Islands',
+    population: 58413
+  },
+  {
+    country: 'Middle income',
+    population: 5678540888
+  },
+  {
+    country: 'North Macedonia',
+    population: 2082958
+  },
+  {
+    country: 'Mali',
+    population: 19077690
+  },
+  {
+    country: 'Malta',
+    population: 484630
+  },
+  {
+    country: 'Myanmar',
+    population: 53708395
+  },
+  {
+    country: 'Middle East & North Africa (excluding high income)',
+    population: 382896715
+  },
+  {
+    country: 'Montenegro',
+    population: 622227
+  },
+  {
+    country: 'Mongolia',
+    population: 3170208
+  },
+  {
+    country: 'Northern Mariana Islands',
+    population: 56882
+  },
+  {
+    country: 'Mozambique',
+    population: 29495962
+  },
+  {
+    country: 'Mauritania',
+    population: 4403319
+  },
+  {
+    country: 'Mauritius',
+    population: 1265303
+  },
+  {
+    country: 'Malawi',
+    population: 18143315
+  },
+  {
+    country: 'Malaysia',
+    population: 31528585
+  },
+  {
+    country: 'North America',
+    population: 364290258
+  },
+  {
+    country: 'Namibia',
+    population: 2448255
+  },
+  {
+    country: 'New Caledonia',
+    population: 284060
+  },
+  {
+    country: 'Niger',
+    population: 22442948
+  },
+  {
+    country: 'Nigeria',
+    population: 195874740
+  },
+  {
+    country: 'Nicaragua',
+    population: 6465513
+  },
+  {
+    country: 'Netherlands',
+    population: 17231624
+  },
+  {
+    country: 'Norway',
+    population: 5311916
+  },
+  {
+    country: 'Nepal',
+    population: 28087871
+  },
+  {
+    country: 'Nauru',
+    population: 12704
+  },
+  {
+    country: 'New Zealand',
+    population: 4841000
+  },
+  {
+    country: 'OECD members',
+    population: 1303529456
+  },
+  {
+    country: 'Oman',
+    population: 4829483
+  },
+  {
+    country: 'Other small states',
+    population: 30758989
+  },
+  {
+    country: 'Pakistan',
+    population: 212215030
+  },
+  {
+    country: 'Panama',
+    population: 4176873
+  },
+  {
+    country: 'Peru',
+    population: 31989256
+  },
+  {
+    country: 'Philippines',
+    population: 106651922
+  },
+  {
+    country: 'Palau',
+    population: 17907
+  },
+  {
+    country: 'Papua New Guinea',
+    population: 8606316
+  },
+  {
+    country: 'Poland',
+    population: 37974750
+  },
+  {
+    country: 'Pre-demographic dividend',
+    population: 919485393
+  },
+  {
+    country: 'Puerto Rico',
+    population: 3195153
+  },
+  {
+    country: 'Korea, Dem. People’s Rep.',
+    population: 25549819
+  },
+  {
+    country: 'Portugal',
+    population: 10283822
+  },
+  {
+    country: 'Paraguay',
+    population: 6956071
+  },
+  {
+    country: 'West Bank and Gaza',
+    population: 4569087
+  },
+  {
+    country: 'Pacific island small states',
+    population: 2457367
+  },
+  {
+    country: 'Post-demographic dividend',
+    population: 1109997273
+  },
+  {
+    country: 'French Polynesia',
+    population: 277679
+  },
+  {
+    country: 'Qatar',
+    population: 2781677
+  },
+  {
+    country: 'Romania',
+    population: 19466145
+  },
+  {
+    country: 'Russian Federation',
+    population: 144478050
+  },
+  {
+    country: 'Rwanda',
+    population: 12301939
+  },
+  {
+    country: 'South Asia',
+    population: 1814388744
+  },
+  {
+    country: 'Saudi Arabia',
+    population: 33699947
+  },
+  {
+    country: 'Sudan',
+    population: 41801533
+  },
+  {
+    country: 'Senegal',
+    population: 15854360
+  },
+  {
+    country: 'Singapore',
+    population: 5638676
+  },
+  {
+    country: 'Solomon Islands',
+    population: 652858
+  },
+  {
+    country: 'Sierra Leone',
+    population: 7650154
+  },
+  {
+    country: 'El Salvador',
+    population: 6420744
+  },
+  {
+    country: 'San Marino',
+    population: 33785
+  },
+  {
+    country: 'Somalia',
+    population: 15008154
+  },
+  {
+    country: 'Serbia',
+    population: 6982604
+  },
+  {
+    country: 'Sub-Saharan Africa (excluding high income)',
+    population: 1078209758
+  },
+  {
+    country: 'South Sudan',
+    population: 10975920
+  },
+  {
+    country: 'Sub-Saharan Africa',
+    population: 1078306520
+  },
+  {
+    country: 'Small states',
+    population: 40575321
+  },
+  {
+    country: 'Sao Tome and Principe',
+    population: 211028
+  },
+  {
+    country: 'Suriname',
+    population: 575991
+  },
+  {
+    country: 'Slovak Republic',
+    population: 5446771
+  },
+  {
+    country: 'Slovenia',
+    population: 2073894
+  },
+  {
+    country: 'Sweden',
+    population: 10175214
+  },
+  {
+    country: 'Eswatini',
+    population: 1136191
+  },
+  {
+    country: 'Sint Maarten (Dutch part)',
+    population: 40654
+  },
+  {
+    country: 'Seychelles',
+    population: 96762
+  },
+  {
+    country: 'Syrian Arab Republic',
+    population: 16906283
+  },
+  {
+    country: 'Turks and Caicos Islands',
+    population: 37665
+  },
+  {
+    country: 'Chad',
+    population: 15477751
+  },
+  {
+    country: 'East Asia & Pacific (IDA & IBRD countries)',
+    population: 2056064424
+  },
+  {
+    country: 'Europe & Central Asia (IDA & IBRD countries)',
+    population: 459865205
+  },
+  {
+    country: 'Togo',
+    population: 7889094
+  },
+  {
+    country: 'Thailand',
+    population: 69428524
+  },
+  {
+    country: 'Tajikistan',
+    population: 9100837
+  },
+  {
+    country: 'Turkmenistan',
+    population: 5850908
+  },
+  {
+    country: 'Latin America & the Caribbean (IDA & IBRD countries)',
+    population: 625569713
+  },
+  {
+    country: 'Timor-Leste',
+    population: 1267972
+  },
+  {
+    country: 'Middle East & North Africa (IDA & IBRD countries)',
+    population: 378327628
+  },
+  {
+    country: 'Tonga',
+    population: 103197
+  },
+  {
+    country: 'South Asia (IDA & IBRD)',
+    population: 1814388744
+  },
+  {
+    country: 'Sub-Saharan Africa (IDA & IBRD countries)',
+    population: 1078306520
+  },
+  {
+    country: 'Trinidad and Tobago',
+    population: 1389858
+  },
+  {
+    country: 'Tunisia',
+    population: 11565204
+  },
+  {
+    country: 'Turkey',
+    population: 82319724
+  },
+  {
+    country: 'Tuvalu',
+    population: 11508
+  },
+  {
+    country: 'Tanzania',
+    population: 56318348
+  },
+  {
+    country: 'Uganda',
+    population: 42723139
+  },
+  {
+    country: 'Ukraine',
+    population: 44622516
+  },
+  {
+    country: 'Upper middle income',
+    population: 2655635719
+  },
+  {
+    country: 'Uruguay',
+    population: 3449299
+  },
+  {
+    country: 'United States',
+    population: 326687501
+  },
+  {
+    country: 'Uzbekistan',
+    population: 32955400
+  },
+  {
+    country: 'St. Vincent and the Grenadines',
+    population: 110210
+  },
+  {
+    country: 'Venezuela, RB',
+    population: 28870195
+  },
+  {
+    country: 'British Virgin Islands',
+    population: 29802
+  },
+  {
+    country: 'Virgin Islands (U.S.)',
+    population: 106977
+  },
+  {
+    country: 'Vietnam',
+    population: 95540395
+  },
+  {
+    country: 'Vanuatu',
+    population: 292680
+  },
+  {
+    country: 'World',
+    population: 7594270356
+  },
+  {
+    country: 'Samoa',
+    population: 196130
+  },
+  {
+    country: 'Kosovo',
+    population: 1845300
+  },
+  {
+    country: 'Yemen, Rep.',
+    population: 28498687
+  },
+  {
+    country: 'South Africa',
+    population: 57779622
+  },
+  {
+    country: 'Zambia',
+    population: 17351822
+  },
+  {
+    country: 'Zimbabwe',
+    population: 14439018
+  }
+]
