97752628ed98f9d326ebf4a71ec4ef46dcad9982
Elin Lütz <lutz.elin@gmail.com>
2020-05-13 14:13:50 +0200

    Update

--- src/components/markers/Markers.js
+++ src/components/markers/Markers.js
@@ -67,7 +67,7 @@ const Markers = ({ onClick }) => {
       color = colors[colorName]
     }
 
-    radius = 5 + 18 * Math.sqrt(number / maxRegionConf / Math.PI)
+    radius = 5 + 50 * Math.sqrt(number / maxRegionConf / Math.PI)
 
     return { color, radius }
   }
